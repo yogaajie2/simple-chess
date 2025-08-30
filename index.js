6b68f714aa8ecfd824349309c3510e5fbfd868b8
yogaajie2 <yogaajie2@gmail.com>
2025-08-30 14:59:16 +0700

    feat: add text input

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const width = 8;
-let playerTurn = "white";
+let playerTurn = "White (lowercase pieces)";
 
 // prettier-ignore
 const startPieces = [
@@ -19,6 +26,7 @@ let board = [];
 function createBoard() {
   for (let row = 0; row < width; row++) {
     board[row] = [];
+
     for (let col = 0; col < width; col++) {
       board[row][col] = startPieces[row * width + col];
     }
@@ -29,20 +37,36 @@ function createBoard() {
 function printBoard() {
   console.log("\n   a b c d e f g h");
   console.log("  -----------------");
+
   for (let row = 0; row < width; row++) {
     let rowStr = 8 - row + " |";
+
     for (let col = 0; col < width; col++) {
       rowStr += (board[row][col] || ".") + " ";
     }
+
     console.log(rowStr + "|" + (8 - row));
   }
+
   console.log("  -----------------");
   console.log("   a b c d e f g h\n");
 }
 
-// --- Game loop
+// Game loop
 function nextTurn() {
   printBoard();
+
+  rl.question(`${playerTurn}'s move (e.g., a2 a3): `, (answer) => {
+    const [from, to] = answer.split(" ");
+
+    // Input format validation
+    if (!from || !to) {
+      console.log("Invalid input. Try again.");
+      return nextTurn();
+    }
+
+    nextTurn();
+  });
 }
 
 // Start game
