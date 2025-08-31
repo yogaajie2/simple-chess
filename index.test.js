6f0c25c0ffb934ffb88039db07c1af49cab6c1ed
yogaajie2 <yogaajie2@gmail.com>
2025-08-31 13:58:09 +0700

    test: add pawn movement testing

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const { beforeEach, describe, expect, test } = require("@jest/globals");
-const { createBoard, board, width } = require("./index");
+const { createBoard, board, width, validatePawnMove } = require("./index");
 
 describe("Board Initialization", () => {
   beforeEach(() => {
@@ -32,5 +32,59 @@ describe("Board Initialization", () => {
 
   test("black back rank is correct", () => {
     expect(board[7]).toEqual(["r", "n", "b", "k", "q", "b", "n", "r"]);
+  });
+});
+
+describe("Pawn Movement", () => {
+  beforeEach(() => {
+    createBoard();
+  });
+
+  test("white pawn can move one step forward", () => {
+    expect(validatePawnMove("p", [6, 0], [5, 0])).toBe(true); // a2 -> a3
+  });
+
+  test("white pawn can move two steps from start", () => {
+    expect(validatePawnMove("p", [6, 0], [4, 0])).toBe(true); // a2 -> a4
+  });
+
+  test("white pawn cannot move two steps if not on start row", () => {
+    expect(validatePawnMove("p", [5, 0], [3, 0])).toBe(false); // a3 -> a5
+  });
+
+  test("white pawn cannot move backwards", () => {
+    expect(validatePawnMove("p", [6, 0], [7, 0])).toBe(false); // a2 -> a1
+  });
+
+  test("white pawn cannot move sideways", () => {
+    expect(validatePawnMove("p", [6, 0], [6, 1])).toBe(false); // a2 -> b2
+  });
+
+  test("white pawn can capture diagonally", () => {
+    // Put black pawn at b3 (row 5, col 1)
+    board[5][1] = "P";
+    expect(validatePawnMove("p", [6, 0], [5, 1])).toBe(true); // a2 -> b3
+  });
+
+  test("white pawn cannot move diagonally into empty square", () => {
+    expect(validatePawnMove("p", [6, 0], [5, 1])).toBe(false); // a2 -> b3
+  });
+
+  test("black pawn can move one step forward", () => {
+    expect(validatePawnMove("P", [1, 0], [2, 0])).toBe(true); // a7 -> a6
+  });
+
+  test("black pawn can move two steps from start", () => {
+    expect(validatePawnMove("P", [1, 0], [3, 0])).toBe(true); // a7 -> a5
+  });
+
+  test("black pawn cannot move backwards", () => {
+    expect(validatePawnMove("P", [1, 0], [0, 0])).toBe(false); // a7 -> a8
+  });
+
+  test("black pawn can capture diagonally", () => {
+    // Put white pawn at b6 (row 2, col 1)
+    board[2][1] = "p";
+    expect(validatePawnMove("P", [1, 0], [2, 1])).toBe(true); // a7 -> b6
   });
 });
